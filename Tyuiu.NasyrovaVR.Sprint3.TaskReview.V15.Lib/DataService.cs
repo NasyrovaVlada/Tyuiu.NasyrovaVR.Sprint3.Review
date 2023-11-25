051586b34a1383d7d2c98192b273044f6b184254
NasyrovaVlada <Tyuiu.NasyrovaVR@outlook.com>
2023-11-25 10:56:28 +0500

    Add project files.

---
+++ Tyuiu.NasyrovaVR.Sprint3.TaskReview.V15.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.NasyrovaVR.Sprint3.TaskReview.V15.Lib
+{
+    public class DataService : ISprint3Task7V15
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            int x;
+            for (x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round((Math.Sin(x) + (2 * x) * Math.Pow(3, -1) - Math.Cos(x)*4*x), 2);
+                valueArray[count] = y;
+                count++;
+                if (y == double.NaN || y == double.PositiveInfinity || y == double.NegativeInfinity)
+                {
+                    y = 0;
+                }
+            }
+
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.NasyrovaVR.Sprint3.TaskReview.V15.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NasyrovaVR.Sprint3.TaskReview.V15.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NasyrovaVR.Sprint3.TaskReview.V15.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0a746cee-3b63-46b3-9c3f-787a62c8c715")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
